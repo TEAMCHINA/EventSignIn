18661c943e59d82ed3831f38868234cb698a1da8
TEAMCHINA <ckchinn8@hotmail.com>
2013-08-21 21:27:56 -0700

    Entity Framework and Categories

---
+++ EventSignIn/Controllers/CategoryController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EventSignIn.DataAccess;
+using EventSignIn.Models;
+
+namespace EventSignIn.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryDataAccess _categoryDataAccess = new CategoryDataAccess();
+
+        public ActionResult Index()
+        {
+            var categories = _categoryDataAccess.GetCategories();
+            return View(categories);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CategoryModel category)
+        {
+            try
+            {
+                _categoryDataAccess.AddCategory(category);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var category = _categoryDataAccess.GetCategoryById(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(CategoryModel category)
+        {
+            try
+            {
+                _categoryDataAccess.UpdateCategory(category);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EventSignIn/DataAccess/CategoryDataAccess.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EventSignIn.Models;
+
+namespace EventSignIn.DataAccess
+{
+    public class CategoryDataAccess
+    {
+        public IList<CategoryModel> GetCategories()
+        {
+            using (var db = new EventSignInEntities())
+            {
+                var categories = db.Categories
+                                   .Select(category => new CategoryModel
+                                       {
+                                           Id = category.Id,
+                                           Name = category.Name,
+                                           Description = category.Description,
+                                       });
+
+                return categories.ToList();
+            }
+        }
+
+        public CategoryModel GetCategoryById(int id)
+        {
+            return GetCategories().FirstOrDefault(category => category.Id == id);
+        }
+
+        public void AddCategory(CategoryModel category)
+        {
+            using (var db = new EventSignInEntities())
+            {
+                db.Categories.Add(new Category
+                    {
+                        Name = category.Name,
+                        Description = category.Description
+                    });
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateCategory(CategoryModel category)
+        {
+            using (var db = new EventSignInEntities())
+            {
+                var dbCategory = db.Categories.FirstOrDefault(c => c.Id == category.Id);
+
+                if (dbCategory == null)
+                {
+                    throw new InvalidOperationException(string.Format("Could not find Category with ID: {0}", category.Id));
+                }
+
+                dbCategory.Name = category.Name;
+                dbCategory.Description = category.Description;
+                db.SaveChanges();
+            }
+        }
+    }
+}
